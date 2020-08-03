c9786660904c87656640591dc48f66e9d93e0a2f
tuyuribr <git@tadeu.work>
2020-08-03 17:52:27 -0300

    licensing

--- config.php
+++ config.php
@@ -1,4 +1,21 @@
 <?php
+/*
+AlterWhats.
+Copyright (C) 2020  yuri at tadeu dot work
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 ini_set('memory_limit', '512M'); // change to your needs
 
 date_default_timezone_set('UTC'); // make that will be 0 always ( for inserting messages )
